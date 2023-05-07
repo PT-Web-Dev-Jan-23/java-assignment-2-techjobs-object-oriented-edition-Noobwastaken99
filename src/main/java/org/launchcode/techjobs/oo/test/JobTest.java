1ee51911b1e14cf00b684626c9dff0677bde0f13
Noobwastaken99 <czmoni99@yahoo.com>
2023-05-06 21:05:53 -0500

    deleted unintentional 'git' typing

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -16,7 +16,7 @@ public class JobTest {
     @Test
     public void testSettingJobId() {
         Job job1 = new Job();
-        Job job2 = new Job();git
+        Job job2 = new Job();
         assertNotEquals(job1.getId(), job2.getId());
     }
 
